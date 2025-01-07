d59d64a90add688ef522f4ddf4f23731686fb65a
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2025-01-07 14:58:58 +0100

    Create a Reusable Card Component

--- alx-project-2/pages/home.tsx
+++ alx-project-2/pages/home.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import Card from '@/components/common/Card'
+import { CardProps } from '@/interfaces';
+import React from 'react';
 
-const home = () => {
+const home: React.FC<CardProps> = () => {
+  const cardsData = [
+      { title: "Card 1", content: "This is the content of card 1." },
+      { title: "Card 2", content: "This is the content of card 2." },
+      { title: "Card 3", content: "This is the content of card 3." },
+  ];
   return (
-    <div>
-      <h1>This is the home page</h1>
-    </div>
-  )
+      <div>
+          <h1>This is the home page</h1>
+
+          {cardsData.map((card, index) => (
+              <div key={index}>
+                  <Card title={card.title} content={card.content} />
+              </div>
+          ))}
+      </div>
+  );
 }
 
 export default home
