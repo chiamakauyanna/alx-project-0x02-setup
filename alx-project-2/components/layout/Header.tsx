15ba7ea13fa46c908189d84ae5d9b610a0ffc4b8
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2025-01-07 14:24:42 +0100

    Basic Routing in Next.js

---
+++ alx-project-2/components/layout/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Link from "next/link"; 
+
+
+const Header = () => {
+  return (
+    <div>
+     <ul>
+      <li>
+        <Link href='/home'>Home</Link>
+      </li>
+      <li>
+        <Link href='/about'>About</Link>
+        </li>
+     </ul>
+    </div>
+  )
+}
+
+export default Header
